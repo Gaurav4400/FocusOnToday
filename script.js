17fc96c993c1812853b6bc63b9d31b5cb1108d03
Gaurav <kumargaurav.2sr@gmail.com>
2024-11-07 00:10:57 +0530

    day 3

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-let checkbox=document.querySelectorAll(".check-box");
-let img
-checkbox.forEach((item)=>{
-    item.addEventListener("click",()=>{
-        
-    })
-})
+let checkboxList = document.querySelectorAll(".check-box");
+let errorp=document.querySelector(".error");
+let input = document.querySelectorAll("input");
+
+
+checkboxList.forEach((checkbox) => {
+  checkbox.addEventListener("click", (e) => {
+    let input = document.querySelectorAll("input");
+    let inputEmpty = false;
+    input.forEach((inp) => {
+        console.log(inp.value);
+        if (inp.value.length == 0) {
+          inputEmpty = true;
+        }
+      });
+
+    if (inputEmpty) {
+      errorp.style.display = "block";
+    } else {
+        errorp.style.display = "none";
+      checkbox.parentElement.classList.toggle("completed");
+    }
+  });
+});
+
+input.forEach((inp) => {
+  inp.addEventListener("input", (e) => {
+        if(inp.value.length==0){
+            checkboxList.forEach((checkbox) => {
+                checkbox.parentElement.classList.remove("completed");
+            })
+        }
+    });
+});
+ 
